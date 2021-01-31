5390bebf935abcf60fbebb4e3de351a75d3d7898
biswajitdixit <buntyoec1998@gmail.com>
2021-01-31 15:45:34 +0530

    ADDED: edit the contact

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,6 +1,4 @@
 package com.add;
-
-
 import java.util.*;
 
 public class AddressBookMain {
@@ -15,7 +13,7 @@ public class AddressBookMain {
         String lastName = sc.next();
         System.out.println("Enter the Address");
         String address = sc.next();
-        System.out.println("Enter the City");						//getting from user input using console
+        System.out.println("Enter the City");
         String city = sc.next();
         System.out.println("Enter the State");
         String state = sc.next();
@@ -27,27 +25,52 @@ public class AddressBookMain {
         return new ContactOfPerson(firstName, lastName, address, city, state, zipCode, phoneNumber);
     }
 
+    static ContactOfPerson edit(ContactOfPerson ContactDetails) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Address");
+        ContactDetails.address = sc.next();
+        System.out.println("Enter the City");
+        ContactDetails.city = sc.next();
+        System.out.println("Enter the State");
+        ContactDetails.state = sc.next();
+        System.out.println("Enter the ZipCode");
+        ContactDetails.zip = sc.next();
+        System.out.println("Enter the contact number...");
+        ContactDetails.phoneNo = sc.next();
+        return ContactDetails;
+
+    }
 
 
-    //menu baar
     static void menu() {
         ContactOfPerson ContactDetails = null;
         int choice;
         do {
             Scanner sc = new Scanner(System.in);
-            System.out.println("Enter ur choice \n 1 : Add \n 2 :  Display ");
+            System.out.println("Enter ur choice \n 1 : Add \n 2 : Edit \n 3 : Display ");
             choice = sc.nextInt();
             switch (choice) {
                 case 1:
                     ContactDetails = add();
                     break;
                 case 2:
+
+                    System.out.println("Enter the ContactDetails Name to edit details..");
+                    String firstName = sc.next();
+                    if (firstName.equals(ContactDetails.firstName)) {
+                        ContactDetails = edit(ContactDetails);
+                    } else {
+                        System.out.println(firstName + " is not exists ");
+                    }
+                    break;
+                case 3:
                     System.out.println(ContactDetails);
                     break;
                 default:
-                    System.out.println("Enter  from 1 to 2");
+                    System.out.println("Enter num from 1 to 3");
             }
-        } while (choice<2);
+        } while (choice<4);
     }
 
     public static void main(String[] args) {
