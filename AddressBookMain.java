cc537af360e3dbfa19c7048a100cf50e84bfb5ba
biswajitdixit <buntyoec1998@gmail.com>
2021-02-24 20:11:55 +0530

    ADDED: count person by state and city name

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -22,7 +22,7 @@ class AddressBookMain {
             switch (option)
             {
                 case 1:
-                        addressBook.addContactDetails();
+                    addressBook.addContactDetails();
                     break;
 
                 case 2:
@@ -95,18 +95,54 @@ class AddressBookMain {
         }
     }
 
+    public void CountByState(String state) {
+        int count = 0;
+        for(Map.Entry<String, AddressBook> entry: addressBookListMap.entrySet()){
+            for(int i=0;i<(entry.getValue()).contactList.size();i++)
+            {
+                ContactOfPerson contact= entry.getValue().contactList.get(i);
+
+                if(state.equals(contact.getState()))
+                {
+                    count++;
+                }
+
+            }
+        }
+        System.out.println("Total Person Count in state "+state+": "+count);
+    }
+    public void CountByCity(String city) {
+        int countPersonInCity=0;
+        for(Map.Entry<String, AddressBook> entry: addressBookListMap.entrySet())
+        {
+            for(int i=0;i<(entry.getValue()).contactList.size();i++)
+            {
+                ContactOfPerson d= entry.getValue().contactList.get(i);
+
+                if(city.equals(d.getCity()))
+                {
+                    countPersonInCity++;
+                }
+
+            }
+        }
+        System.out.println("Total number of people in this city "+city+": "+countPersonInCity);
+    }
+
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the Address Book Management System using Java Stream");
+        System.out.println("----------------****Welcome to the Address Book System****--------------");
         AddressBookMain addressBookMain = new AddressBookMain();
         boolean flag =true;
         while(flag) {
             System.out.println("1.Add New Address Book");
             System.out.println("2.Search Contact from a city");
             System.out.println("3.Search Contact from a State");
-            System.out.println("4.View contact By State Using State and Person HashMap");
-            System.out.println("5.View Contact by city Using City and Person HashMap");
-            System.out.println("6.Exit");
+            System.out.println("4.View contact By State ");
+            System.out.println("5.View Contact by city ");
+            System.out.println("6.Count Contact By State");
+            System.out.println("7.Count Contact By City");
+            System.out.println("8.Exit");
 
             System.out.println("Enter choice: ");
             int option = sc.nextInt();
@@ -116,6 +152,7 @@ class AddressBookMain {
                     String addressBookName = sc.next();
                     if (addressBookListMap.containsKey(addressBookName)) {
                         System.out.println("The Address book Already Exists");
+                        addressBookMain.addAddressBook(addressBookName);
                         break;
                     } else {
                         addressBookMain.addAddressBook(addressBookName);
@@ -146,8 +183,20 @@ class AddressBookMain {
                     String cityName1 = sc.next();
                     addressBookMain.viewPersonByCityUsingHashMap(cityName1);
                     break;
-
                 case 6:
+                    System.out.println("Enter Name of State: ");
+                    String stateName2 = sc.next();
+                    addressBookMain.CountByState(stateName2);
+                    break;
+
+                case 7:
+                    System.out.println("Enter Name of City: ");
+                    String cityName2 = sc.next();
+                    addressBookMain.CountByCity(cityName2);
+                    break;
+
+
+                case 8:
                     flag = false;
                     break;
             }
